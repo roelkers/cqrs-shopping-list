0b90e919f3fe02e9580849de654157c905dcd559
roelkers <rufus.oelkers@gmail.com>
2020-05-17 16:07:40 +0200

    connect to heroku postgresql db on prod

--- server/src/pgClient.ts
+++ server/src/pgClient.ts
@@ -2,12 +2,16 @@ import keys from "./keys";
 
 // Postgres Client Setup
 const { Pool } = require("pg");
-const pgClient = new Pool({
-  user: keys.pgUser,
-  host: keys.pgHost,
-  database: keys.pgDatabase,
-  password: keys.pgPassword,
-  port: keys.pgPort
-});
+const pgClient = process.env.DATABASE_URL ?
+  new Pool({
+    connectionString: process.env.DATABASE_URL
+  }) :
+  new Pool({
+    user: keys.pgUser,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    password: keys.pgPassword,
+    port: keys.pgPort
+  });
 
 export default pgClient;
